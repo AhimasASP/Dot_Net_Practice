4a90a1a5068321afd95994ba8405fdcb81421d6a
Alex <potetenko.alexey@gmail.com>
2019-10-03 20:02:19 +0300

    All Tasks in PZ_06 done

---
+++ ClassWork_05/Task_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            int maxValue = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(1, 100);
+                //Console.WriteLine("Please enter array element number " + (i + 1));
+                //array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("element number " + (i + 1));
+                Console.Write(array[i] + "\n");
+            }
+            array[2] *= 10;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("element number " + (i + 1));
+                Console.Write(array[i] + "\n");
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+            }
+            Console.WriteLine("Max value is: " + maxValue);
+
+        }
+    }
+}
---
+++ ClassWork_06/PZ_06/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace PZ_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Empty array creation is possible!\nLook at there!\n");
+            program.ShowArray(program.EmptyArrayCreation());
+            program.ObjectArrayCreation();
+            Console.WriteLine(program.RandomArrayCreation());
+
+        }
+
+        public void ShowArray(int[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                builder.Append(array[i]);
+                builder.Append(", ");
+            }
+            Console.WriteLine("[" + builder.ToString().Substring(0, builder.ToString().Length - 2) + "]");
+        }
+
+        public int[] EmptyArrayCreation()
+        {
+            return new int[10];
+        }
+
+        public void ObjectArrayCreation()
+        {
+            object[] objArray;
+            Console.WriteLine(objArray = new object[3]{32, 'A', "Hello"});
+            objArray[0] = Convert.ToInt32(objArray[0]) + 10;
+            objArray[2] = Convert.ToString(objArray[2]) + ", guys!";
+            foreach (object obj in objArray)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+        }
+
+        public int RandomArrayCreation()
+        {
+            int maxValue = 0;
+            int[] randomArray = new int[13];
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = new Random().Next(1, 100);
+                if (randomArray[i] > maxValue) maxValue = randomArray[i];
+            }
+            ShowArray(randomArray);
+            return maxValue;
+        }
+        
+    }
+
+
+
+
+}
